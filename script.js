c25b69e52e044ba228bee5e225725a67d8876360
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-02-07 10:15:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.getElementById('fname').addEventListener('blur', function() {
+    let inputField = document.getElementById('fname');
+    inputField.value = inputField.value.toUpperCase();
+});
